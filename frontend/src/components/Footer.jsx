12aefd127231b032409ec797c239d096cb6c3824
NaveedAfraz <naveedafraz123@gmail.com>
2025-03-31 12:18:01 +0530

    changed the data

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
     <footer className="py-6 md:py-12 border-t">
       <div className="container px-4 md:px-6 mx-auto">
         <div className="flex flex-col items-center justify-center gap-4 md:gap-6">
-          <p className="text-center text-sm text-muted-foreground">
+          {/* <p className="text-center text-sm text-muted-foreground">
             {new Date().getFullYear()}
-          </p>
+          </p> */}
 
           <div className="text-center">
             <p className="animated-gradient-text text-center text-sm font-medium">
