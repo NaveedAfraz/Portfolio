1ad14aacbd06f8b758ea9f814d584de23aec0444
NaveedAfraz <naveedafraz123@gmail.com>
2025-04-18 02:15:23 +0530

    added resume

--- frontend/src/components/sections/Hero.jsx
+++ frontend/src/components/sections/Hero.jsx
@@ -103,7 +103,7 @@ const Hero = () => {
             >
               View My Work
             </button>
-            <a href="/NaveedAfrazResume.pdf" download="NaveedAfrazResume.pdf">
+            <a href="/NaveedAfrazResume..pdf" download="NaveedAfrazResume..pdf">
               <button className="cursor-pointer px-6 py-3 hover:scale-105 transition-transform rounded-md border border-gray-300 dark:border-gray-700 font-medium">
                 Download Resume
               </button>
